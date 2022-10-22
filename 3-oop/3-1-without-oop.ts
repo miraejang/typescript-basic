8bda544c789212af9d359f4dcd1c049c5b3c083f
miraejang <mirae611@nate.com>
2022-10-22 23:06:22 +0900

    Imperative and Procedural Programming

---
+++ 3-oop/3-1-without-oop.ts
@@ -0,0 +1,26 @@
+{
+  // Imperative and Procedural Programming
+
+  type CoffeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  const BEANS_GRAMM_PER_SHOT: number = 7;
+  let coffeeBeans: number = 0;
+
+  function makeCoffee(shots: number): CoffeCup {
+    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+      throw new Error('not enough coffee beans!');
+    }
+    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
+    return {
+      shots,
+      hasMilk: false,
+    };
+  }
+
+  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  const coffee = makeCoffee(2);
+  console.log(coffee);
+}
