ef487a6a7fc7a0dd49c64c1a3299527f2e4eb644
miraejang <mirae611@nate.com>
2022-10-25 01:23:55 +0900

    Abstract Class

---
+++ 3-oop/3-8-abstract.ts
@@ -0,0 +1,96 @@
+{
+  // Absctract Class
+  /**
+   * abstract class 자체로는 object를 만들 수 없음
+   * 구현하는 class마다 달라져야하는 내용이있다면 abstract method로 정의할 수 있음
+   * abstract method는 구현하는 code를 작성하지 않음
+   */
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  abstract class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('Cleaning the machine... 🧼');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`Grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('Heating up... 🔥');
+    }
+
+    protected abstract extract(shots: number): CoffeeCup;
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk(): void {
+      console.log('Steaming some milk... 🥛');
+    }
+
+    protected extract(shots: number): CoffeeCup {
+      this.steamMilk();
+      return {
+        shots,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMachine extends CoffeeMachine {
+    protected extract(shots: number): CoffeeCup {
+      return {
+        shots,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    new CaffeLatteMachine(16, 'a'),
+    new SweetCoffeeMachine(16),
+    new CaffeLatteMachine(16, 'a'),
+    new SweetCoffeeMachine(16),
+  ];
+  machines.forEach(machine => {
+    console.log('----------------------');
+    machine.makeCoffee(1);
+  });
+}
